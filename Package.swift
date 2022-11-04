7a17da72cebf1ca5c4e5851712c0e347490dad57
Stijn Willems <stijn@dooz.be>
2022-11-04 16:58:59 +0100

    Uses swiftlint repo link instead of binary

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "AirbnbSwift",
-  platforms: [.macOS(.v10_13)],
+  platforms: [.macOS(.v12)],
   products: [
     .plugin(name: "FormatSwift", targets: ["FormatSwift"]),
   ],
@@ -12,6 +12,7 @@ let package = Package(
     // points to https://github.com/calda/SwiftFormat/releases/tag/0.51-beta-3 as the tag could not be used in that format
     // swiftformat is included directly to be able to support linux too
     .package(url: "https://github.com/calda/SwiftFormat", revision: "b9aa1814747d37c86fd1553d8dfefbe7557f8c34"),
+    .package(url: "https://github.com/realm/SwiftLint", branch: "main"),
   ],
   targets: [
     .plugin(
@@ -26,7 +27,7 @@ let package = Package(
       dependencies: [
         "AirbnbSwiftFormatTool",
         .product(name: "swiftformat", package: "SwiftFormat"),
-        "SwiftLintBinary",
+        .product(name: "swiftlint", package: "SwiftLint"),
       ]),
 
     .executableTarget(
@@ -38,8 +39,4 @@ let package = Package(
         .process("airbnb.swiftformat"),
         .process("swiftlint.yml"),
       ]),
-    .binaryTarget(
-      name: "SwiftLintBinary",
-      url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
-      checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"),
   ])
